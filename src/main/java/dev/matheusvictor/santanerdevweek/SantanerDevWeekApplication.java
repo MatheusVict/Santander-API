b708e97bd557c839f916a3b71f3a6709185d475d
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-24 18:53:55 -0300

    inital commit

---
+++ src/main/java/dev/matheusvictor/santanerdevweek/SantanerDevWeekApplication.java
@@ -0,0 +1,13 @@
+package dev.matheusvictor.santanerdevweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanerDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanerDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheusvictor/santanerdevweek/SantanerDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheusvictor.santanerdevweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanerDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
